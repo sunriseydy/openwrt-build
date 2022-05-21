c65edc69be852105128160cb18cd742e242b4401
SunriseYDY <sunriseydy@gmail.com>
2022-05-21 20:52:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# add openclash
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
